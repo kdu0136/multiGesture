a36b734f013c15cabf7c5996054bc88c2de23c3e
dongun kim <>
2020-06-05 18:14:10 +0900

    - Add sample gesture source code.

--- viewZoom/src/main/java/kim/dongun/viewZoom/GestureCopyView.kt
+++ viewZoom/src/main/java/kim/dongun/viewZoom/GestureCopyView.kt
@@ -1,15 +1,25 @@
 package kim.dongun.viewZoom
 
+import android.annotation.SuppressLint
+import android.content.ClipData
+import android.content.ClipDescription
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Matrix
 import android.graphics.PointF
+import android.graphics.RectF
+import android.os.Build
 import android.util.AttributeSet
+import android.util.Log
 import android.view.GestureDetector
 import android.view.GestureDetector.SimpleOnGestureListener
 import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 import android.view.ScaleGestureDetector.SimpleOnScaleGestureListener
 import android.view.View
+import androidx.core.view.ViewCompat
+import kotlin.math.max
+import kotlin.math.min
 
 /**
  * 뷰 복사 클래스
@@ -18,50 +28,84 @@ class GestureCopyView@JvmOverloads constructor(context: Context,
                                                attrs: AttributeSet? = null,
                                                defStyleAttr: Int = 0) : View(context, attrs, defStyleAttr) {
     private val DOUBLE_TAP_ZOOM_DURATION = 200
+    private var scaleFactor = 1f // zoom scale 값
+    private val minScaleFactor = 1f // min 줌 값
+    private var maxScaleFactor = 2f // max 줌 값
 
-    private class GestureListener : SimpleOnGestureListener() {
-
+    private val gestureListener: SimpleOnGestureListener = object: SimpleOnGestureListener() {
+        override fun onSingleTapUp(e: MotionEvent?): Boolean {
+//            Log.d("Test/Log", "GestureListener onSingleTapUp")
+            return super.onSingleTapUp(e)
+        }
         override fun onDoubleTap(e: MotionEvent?): Boolean {
             e?.run {
 //                zoomImageToPosition(getDoubleTapTargetScale(), e.x, e.y, DOUBLE_TAP_ZOOM_DURATION)
             }
+//            Log.d("Test/Log", "GestureListener onDoubleTap.")
             return super.onDoubleTap(e)
         }
         override fun onScroll(e1: MotionEvent, e2: MotionEvent, distanceX: Float, distanceY: Float
         ): Boolean {
+//            Log.d("Test/Log", "GestureListener onScroll ($distanceX, $distanceY) / view ($x, $y)")
+//            x += -distanceX
+//            y += -distanceY
+//            val offSetX = distanceX * mCurrentViewport.width() / width
+//            val offSetY = -distanceY * mCurrentViewport.height() / height
+//            setViewportBottomLeft(x = mCurrentViewport.left + offSetX, y = mCurrentViewport.bottom + offSetY)
 //            postTranslate(-distanceX, -distanceY)
+            return super.onScroll(e1, e2, distanceX, distanceY)
+        }
+    }
+
+    private val scaleListener: SimpleOnScaleGestureListener = object: SimpleOnScaleGestureListener() {
+        override fun onScale(detector: ScaleGestureDetector?): Boolean {
+            if (detector != null) {
+                scaleFactor *= detector.scaleFactor
+                // 최대 줌 설정
+                scaleFactor = max(minScaleFactor, min(scaleFactor, maxScaleFactor))
+                scaleX = scaleFactor
+                scaleY = scaleFactor
+            }
             return true
         }
     }
 
-    private class ScaleListener : SimpleOnScaleGestureListener() {
-        override fun onScale(detector: ScaleGestureDetector): Boolean {
-//            postScale(detector.scaleFactor, mMidPntX, mMidPntY)
-            return true
+    private val rotateListener: RotationGestureDetector.SimpleOnRotationGestureListener = object: RotationGestureDetector.SimpleOnRotationGestureListener() {
+        override fun onRotation(rotationDetector: RotationGestureDetector?): Boolean {
+            if (rotationDetector != null)
+                rotation += rotationDetector.getAngle()
+            return false
         }
-    }
-
-    private class RotateListener : RotationGestureDetector.SimpleOnRotationGestureListener() {
-        override fun onRotation(rotationDetector: RotationGestureDetector?): Boolean = true
     }
 
     // 복사한 뷰의 source
     private lateinit var source: View
 
-    private val gestureDetector: GestureDetector by lazy { GestureDetector(getContext(), GestureListener(), null, true) }
-    private val scaleDetector: ScaleGestureDetector by lazy { ScaleGestureDetector(getContext(), ScaleListener()) }
-    private val rotateDetector: RotationGestureDetector by lazy { RotationGestureDetector(listener = RotateListener()) }
+    private val gestureDetector: GestureDetector by lazy { GestureDetector(getContext(), gestureListener, null, true) }
+//    private val scaleDetector: ScaleGestureDetector by lazy { ScaleGestureDetector(getContext(), ScaleListener()) }
+    private val scaleDetector: ScaleGestureDetector by lazy { ScaleGestureDetector(getContext(), scaleListener) }
+    private val rotateDetector: RotationGestureDetector by lazy { RotationGestureDetector(listener = rotateListener) }
 
+    private var initialPinchMidPoint: PointF = PointF() // pinch 시작 초기 중심점
     private val midPoint: PointF = PointF()
 
-    var isRotateEnabled: Boolean = true
+    var isRotateEnabled: Boolean = false
     var isScaleEnabled: Boolean = true
     var doubleTapScaleSteps = 5
+    var start = false
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         event?.run {
-            if ((action and MotionEvent.ACTION_MASK) == MotionEvent.ACTION_DOWN) {
-//                cancelAllAnimations() TODO: CREATE FUNC
+//            if ((action and MotionEvent.ACTION_MASK) == MotionEvent.ACTION_DOWN) {
+////                cancelAllAnimations() TODO: CREATE FUNC
+//            }
+            if (action and MotionEvent.ACTION_MASK == MotionEvent.ACTION_MOVE) {
+//                val data = ClipData("", arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN), ClipData.Item(""))
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+//                    startDragAndDrop(data, DragShadowBuilder(this@GestureCopyView), this, 0)
+//                else
+//                    startDrag(data, DragShadowBuilder(this@GestureCopyView), this, 0)
             }
             midPoint.midPointOfEvent(event = this)
             gestureDetector.onTouchEvent(this)
@@ -75,12 +119,32 @@ class GestureCopyView@JvmOverloads constructor(context: Context,
             if ((action and MotionEvent.ACTION_MASK) == MotionEvent.ACTION_UP) {
 //                setImageToWrapCropBounds() TODO: CREATE FUNC
             }
+
+//            when (action and MotionEvent.ACTION_MASK ) {
+//                MotionEvent.ACTION_POINTER_DOWN -> {
+//                    start = true
+//                    initialPinchMidPoint.midPointOfEvent(event = this)
+//                }
+//                MotionEvent.ACTION_MOVE -> {
+//                    if (start) {
+//                        val offSetX = initialPinchMidPoint.x - midPoint.x
+//                        val offSetY = initialPinchMidPoint.y - midPoint.y
+//
+//                        this@GestureCopyView.x -= offSetX
+//                        this@GestureCopyView.y -= offSetY
+//                    }
+//
+//                }
+//                MotionEvent.ACTION_POINTER_UP,
+//                MotionEvent.ACTION_UP,
+//                MotionEvent.ACTION_CANCEL -> { // 한개 또는 두개 포인트에 대한 up 또는 액션 cancel
+//                    start = false
+//                }
+//                else -> {}
+//            }
         }
         return true
     }
-
-//    private fun getDoubleTapTargetScale(): Float =
-//        getCurrentScale() * Math.pow(getMaxScale() / getMinScale(), 1f / doubleTapScaleSteps)
 
     /**
      * 뷰 복사
